cba12a5a8b6d0193748dd20b3b09b22039599463
Moulika95 <115602863+Moulika95@users.noreply.github.com>
2023-08-04 22:41:44 -0400

    Create sqlquery.sql

---
+++ sqlquery.sql
@@ -0,0 +1,6 @@
+select ABS((select max (salary) from  db_employee e join db_dept d on
+e.department_id=d.id 
+      where department='marketing') -
+       
+(select max(Salary) from  db_employee e join db_dept d on e.department_id=d.id  where department='engineering'))  as diff
+       ;
